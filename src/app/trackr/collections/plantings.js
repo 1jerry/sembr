e66d09bf7ebd84d7b208a201a450a1a3caa402b1
Andru Vallance <andru@tinymighty.com>
2014-06-17 20:45:43 +0200

    Added filterByModels method to Plantings collection: filters the collection models to only those which have an association with one of the models passed as the function argument.

--- src/app/trackr/collections/plantings.js
+++ src/app/trackr/collections/plantings.js
@@ -12,6 +12,45 @@ define(["sembr", "underscore", "sembr.collection"],
 
 		  comparator: function(chapter) {
 			  return chapter.get("planted_from");
+			},
+			
+
+			/**
+      * Returns a new Plantings collections filterered by the presence of
+      * the provided models as associations to the planting models
+      */
+			filterByModels: function( models ){
+			  var filtered;
+			  
+			  console.log('Filtering plantings by models %o', models);
+        
+        if( !models || !models.length ){
+          console.warn('No models provided to filter, returning all models');
+          return this;
+        }
+        
+			  filtered = this.filter( function( planting ){
+			    var result = false;
+          _( models ).each( function( model ){
+            switch( model.get('type') ){
+              case 'plant':
+                if( planting.get('plant_id') === model.get('id') )
+                  result = true;
+                break;
+              case 'place':
+                if( planting.get('place_id') === model.get('id') )
+                  result = true;
+                break;
+              default:
+              break;
+            }
+          });
+          
+          return result;
+        });
+        console.log('Filtered plantings %o', filtered);
+			  
+			  return new Plantings( filtered );
 			}
 
 		});
